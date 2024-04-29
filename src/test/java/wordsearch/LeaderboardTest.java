769c788105f9c675c70f04ead5fe388460db5c3c
Jahnavi Aleti <147567141+qwrwtdycuvjbk@users.noreply.github.com>
2024-04-29 14:52:01 -0500

    Create LeaderboardTest.java

---
+++ src/test/java/wordsearch/LeaderboardTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class LeaderBoardTest {
+
+    @Test
+    public void testConstructorAndGetters() {
+       
+        Player player = new Player("John");
+
+
+        LeaderBoard leaderBoard = new LeaderBoard(player, 1000, 1);
+
+        assertEquals("John", leaderBoard.getPlayer().getName());
+        assertEquals(1000, leaderBoard.getPlayerScore());
+        assertEquals(1, leaderBoard.getRank());
+    }
+}
