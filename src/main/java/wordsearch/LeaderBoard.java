10cd5c035960c6b053024a177163f6d1cfabd539
Jahnavi Aleti <147567141+qwrwtdycuvjbk@users.noreply.github.com>
2024-04-29 14:32:11 -0500

    Update LeaderBoard.java

--- src/main/java/wordsearch/LeaderBoard.java
+++ src/main/java/wordsearch/LeaderBoard.java
@@ -3,9 +3,22 @@ package wordsearch;
 public class LeaderBoard {
     private Player player;
     private int playerScore;
+    private int rank;
 
     public LeaderBoard() {
         this.player = player;
         this.playerScore = playerScore;
+        this.rank = rank;
+    }
+    public Player getPlayer() {
+        return player;
+    }
+     
+    public int getPlayerScore() {
+        return playerScore;
+    }
+    
+    public int getRank() {
+        return rank;
     }
 }
